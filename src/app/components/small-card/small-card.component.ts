72b28f909a5d5b0356b35dd3826adae7958172f6
jesus maikon reis figueiredo <rmaikon062@gmail.com>
2024-02-25 18:09:02 -0300

    components dinamycs propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
+
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
 
 }
